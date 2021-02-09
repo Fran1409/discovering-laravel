006d992855086b518a2de5826ee469a59add65c0
Fran1409 <fran_andries@hotmail.com>
2021-02-09 13:09:11 +0100

    Add footer to homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,3 +23,26 @@
         <div class="product-device product-device-2 box-shadow d-none d-md-block"></div>
     </div>
 @endsection
+
+@section('footer')
+<footer class="container py-5">
+    <div class="row">
+      <div class="col-12 col-md">
+        <h6><a href="#">Cocktails</a></h6>        
+        <small class="d-block mb-3 text-muted">&copy; BeCode 2020-2021</small>
+      </div>
+      <div class="col-6 col-md">
+        <h6><a href="#">About us</a></h6>
+      </div>
+      <div class="col-6 col-md">
+        <h6>Contact</h6>
+        <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Team</a></li>
+            <li><a class="text-muted" href="#">Locations</a></li>
+            <li><a class="text-muted" href="#">Privacy</a></li>
+            <li><a class="text-muted" href="#">Terms</a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+@endsection
