faf60dd1b2993937affbed12a5c4b5eebabaf07c
Fran1409 <fran_andries@hotmail.com>
2021-02-10 17:09:48 +0100

    Add working href in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
         <div class="col-md-5 p-lg-5 mx-auto my-5">
             <h1 class="display-4 font-weight-normal" style="font-style: italic;">It's beginning to look a lot like cocktails</h1>
             <p class="lead font-weight-normal">In the mood for some cocktails? Not in the mood to get out of your house? Check out our cocktails and order now!</p>
-            <a class="btn btn-outline-secondary" href="#">Wine not?</a>
+            <a class="btn btn-outline-secondary" href="/cocktails">Wine not?</a>
         </div>
         <div class="product-device box-shadow d-none d-md-block"></div>
         <div class="product-device product-device-2 box-shadow d-none d-md-block"></div>
