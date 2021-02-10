8c1f8c727e19a1544233e02959429761b4966731
Fran1409 <fran_andries@hotmail.com>
2021-02-10 16:44:26 +0100

    Add header and footer in seperate files

---
+++ resources/views/header.blade.php
@@ -0,0 +1,10 @@
+@section('header')
+    <nav class="site-header sticky-top py-1">
+        <div class="container d-flex flex-column flex-md-row justify-content-between">
+            <a class="py-2" href="/">Home</a>
+            <a class="py-2 d-none d-md-inline-block" href="cocktails">Cocktails</a>
+            <a class="py-2 d-none d-md-inline-block" href="about">About us</a>
+            <a class="py-2 d-none d-md-inline-block" href="contact">Contact</a>
+        </div>
+    </nav>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,6 @@
 @extends('layout')
 
-@section('header')
-    <nav class="site-header sticky-top py-1">
-        <div class="container d-flex flex-column flex-md-row justify-content-between">
-            <a class="py-2" href="/">Home</a>
-            <a class="py-2 d-none d-md-inline-block" href="/cocktails">Cocktails</a>
-            <a class="py-2 d-none d-md-inline-block" href="/about">About us</a>
-            <a class="py-2 d-none d-md-inline-block" href="/contact">Contact</a>
-        </div>
-    </nav>
-@endsection
+@extends('header')
 
 @section('content')
 
@@ -24,25 +15,4 @@
     </div>
 @endsection
 
-@section('footer')
-<footer class="container py-5">
-    <div class="row">
-      <div class="col-12 col-md">
-        <h6><a href="#">Cocktails</a></h6>        
-        <small class="d-block mb-3 text-muted">&copy; BeCode 2020-2021</small>
-      </div>
-      <div class="col-6 col-md">
-        <h6><a href="#">About us</a></h6>
-      </div>
-      <div class="col-6 col-md">
-        <h6>Contact</h6>
-        <ul class="list-unstyled text-small">
-            <li><a class="text-muted" href="#">Team</a></li>
-            <li><a class="text-muted" href="#">Locations</a></li>
-            <li><a class="text-muted" href="#">Privacy</a></li>
-            <li><a class="text-muted" href="#">Terms</a></li>
-        </ul>
-      </div>
-    </div>
-  </footer>
-@endsection
+@extends('footer')
