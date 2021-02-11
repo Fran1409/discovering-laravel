9d8eb4810c0df2d2c5e4fcf592f91955325dc2ae
Fran1409 <fran_andries@hotmail.com>
2021-02-11 10:42:27 +0100

    Add picture to order confirmation

--- resources/views/orderConfirmation.blade.php
+++ resources/views/orderConfirmation.blade.php
@@ -4,8 +4,9 @@
 
 @section('content')
     <div class="box">
-        <div class="row">
+        <div class="row-order">
             <h3>Cocktails are on their way!</h3>
+            <img src="../images/cocktailtime.png" alt="cocktailtime">
         </div>
     </div>
 @endsection
